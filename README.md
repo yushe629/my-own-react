9616c055b8814dfe45b2fb3c8e64503343e76260
yushe <yushedev@gmail.com>
2025-05-31 12:24:53 +0900

    add: workloop and fiber

---
+++ README.md
@@ -0,0 +1,8 @@
+# my-own-react
+reactの基本的モジュールを自作する
+
+
+# Usage
+```
+$ npm run dev
+```
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,18 @@ import { MiniReact } from "../scripts/MiniReact.js";
 
 /** @jsx MiniReact.createElement */
 const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-    <div>表示できた</div>
-    <button onClick={() => alert("button")}>ボタン</button>
+  //   <div id="foo">
+  //     <a>bar</a>
+  //     <b />
+  //     <div>表示できた</div>
+  //     <button onClick={() => alert("button")}>ボタン</button>
+  //   </div>
+  <div>
+    <h1>
+      <p />
+      <a />
+    </h1>
+    <h2 />
   </div>
 )
 
